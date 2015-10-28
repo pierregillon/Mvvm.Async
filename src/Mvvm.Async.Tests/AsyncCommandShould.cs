c59758833691a7adb34612f691f60f203a1c18ce
Pierre GILLON <gillon.pierre69@gmail.com>
2015-10-28 10:18:22 +0100

    Implement AsyncCommand

---
+++ src/Mvvm.Async.Tests/AsyncCommandShould.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using NFluent;
+using Xunit;
+
+namespace Mvvm.Async.Tests
+{
+    public class AsyncCommandShould
+    {
+        private static readonly Func<Task> SOME_ACTION = () => Task.Factory.StartNew(() => { });
+
+        [Fact]
+        public async Task execute_an_async_delegate()
+        {
+            var result = string.Empty;
+
+            Func<Task> action = () => Task.Factory.StartNew(() => result = "Success");
+
+            var asyncCommand = new AsyncCommand(action);
+
+            await asyncCommand.ExecuteAsync();
+
+            Check.That(result).IsEqualTo("Success");
+        }
+
+        [Fact]
+        public void not_be_executable_when_predicate_returns_false()
+        {
+            var asyncCommand = new AsyncCommand(SOME_ACTION, () => false);
+
+            var canExecute = asyncCommand.CanExecute();
+
+            Check.That(canExecute).IsFalse();
+        }
+
+        [Fact]
+        public void be_executable_when_predicate_returns_false()
+        {
+            var asyncCommand = new AsyncCommand(SOME_ACTION, () => true);
+
+            var canExecute = asyncCommand.CanExecute();
+
+            Check.That(canExecute).IsTrue();
+        }
+
+        [Fact]
+        public void be_executable_when_no_predicate_defined()
+        {
+            var asyncCommand = new AsyncCommand(SOME_ACTION);
+
+            var canExecute = asyncCommand.CanExecute();
+
+            Check.That(canExecute).IsTrue();
+        }
+    }
+}
--- src/Mvvm.Async/AsyncCommand.cs
+++ src/Mvvm.Async/AsyncCommand.cs
@@ -1,8 +1,37 @@
-﻿using System.Windows.Input;
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Mvvm.Async
 {
-    public class AsyncCommand
+    public class AsyncCommand : ICommand
     {
+        private readonly Func<Task> _action;
+        private readonly Func<bool> _predicate;
+
+        public AsyncCommand(Func<Task> action, Func<bool> predicate = null)
+        {
+            _action = action;
+            _predicate = predicate;
+        }
+        public async Task ExecuteAsync()
+        {
+            await _action();
+        }
+        public bool CanExecute()
+        {
+            return _predicate == null || _predicate();
+        }
+
+        // ----- Implement ICommand
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+        public event EventHandler CanExecuteChanged;
     }
 }
