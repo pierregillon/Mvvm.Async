8544523637273d42092b0c3be13a357f9e0e53d2
Pierre GILLON <gillon.pierre69@gmail.com>
2015-10-28 13:49:17 +0100

    Implement an wpf example for the AsyncCommand.

---
+++ examples/SimpleWpfApplication/ViewModels/MainViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Mvvm.Async;
+
+namespace SimpleWpfApplication.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private int _clickCount;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string _message;
+        public string Message
+        {
+            get { return _message; }
+            set
+            {
+                _message = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ICommand _writeMessageCommand;
+        public ICommand WriteMessageCommand
+        {
+            get { return _writeMessageCommand ?? (_writeMessageCommand = new AsyncCommand(WriteMessageAsync)); }
+        }
+
+        public MainViewModel()
+        {
+            _message = GetTextToDisplay(_clickCount);
+        }
+
+        private Task WriteMessageAsync()
+        {
+            return Task.Factory.StartNew(() => { Message = GetTextToDisplay(++_clickCount); });
+        }
+
+        private static string GetTextToDisplay(int count)
+        {
+            return string.Format("Was clicked : {0} times", count);
+        }
+    }
+}
