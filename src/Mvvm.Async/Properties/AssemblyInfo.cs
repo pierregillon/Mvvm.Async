852c81176dbd59bf1da00fc6c20e2eb58a0c9fd4
Pierre GILLON <gillon.pierre69@gmail.com>
2015-10-29 15:59:06 +0100

    Creation of the nuspec and the first version 0.0.1

--- src/Mvvm.Async/Properties/AssemblyInfo.cs
+++ src/Mvvm.Async/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Mvvm.Async")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Simple library to help use mvvm with async pattern objects.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Betclic")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Mvvm.Async")]
-[assembly: AssemblyCopyright("Copyright © Betclic 2015")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
